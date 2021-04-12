09c98d83f075c5cb5a320e1b0cf5c871a2894431
msbaek <ms.baek@sk.com>
2021-04-12 11:04:49 +0900

    rename spock test class

--- src/test/java/com/example/employee/SpockFeaturesSpec.groovy
+++ src/test/java/com/example/employee/SpockFeaturesSpec.groovy
@@ -5,7 +5,7 @@ import spock.lang.Specification
 /**
  * https://blog.jetbrains.com/idea/2021/01/tutorial-spock-part-1-getting-started/
  */
-class EmployeeControllerSpec extends Specification {
+class SpockFeaturesSpec extends Specification {
     def "should be a simple assertion"() {
         expect:
         1 == 1
